fafdce13e7ba3293ef41ed60b67b18ddc23181c6
Aniket Kumar Verma <vermaaniket213@gmail.com>
2019-12-29 14:49:17 +0530

    cleanup

--- contact_list/src/store/index.js
+++ contact_list/src/store/index.js
@@ -2,8 +2,5 @@ import {createStore,applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducer';
 
-const store=createStore(
-    reducers,
-    applyMiddleware(ReduxThunk)
-)
+const store=createStore(reducers,applyMiddleware(ReduxThunk))
 export default store;
